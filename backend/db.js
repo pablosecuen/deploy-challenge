658af0cffa3a59353104ec69c115493239908e17
pablosecuen <pablosecuen@gmail.com>
2023-12-31 12:20:25 -0300

    db language fix

--- backend/db.js
+++ backend/db.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 let { DB_NAME, DB_USER, DB_PASSWORD, DB_HOST } = process.env;
 
-// Configuración de la conexión a la base de datos
+// Database config
 if (DB_NAME && DB_USER && DB_PASSWORD && DB_HOST) {
   const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
     host: DB_HOST,
@@ -17,6 +17,6 @@ if (DB_NAME && DB_USER && DB_PASSWORD && DB_HOST) {
 
   module.exports = { Note, sequelize };
 } else {
-  console.error("Las variables de entorno de la base de datos no están completamente definidas.");
+  console.error("local variables are not defined");
   process.exit(1);
 }
