0c4dbc062ee7db1d4135693091cb10ff09f610a5
pablosecuen <pablosecuen@gmail.com>
2023-12-31 12:41:13 -0300

    fix por deploy 2

--- backend/db.js
+++ backend/db.js
@@ -1,12 +1,11 @@
-// db.js
-const { Sequelize } = require("sequelize");
 const dotenv = require("dotenv");
 dotenv.config();
 
 let { DB_NAME, DB_USER, DB_PASSWORD, DB_HOST } = process.env;
 
-// Database config
 if (DB_NAME && DB_USER && DB_PASSWORD && DB_HOST) {
+  const { Sequelize } = require("sequelize");
+
   const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
     host: DB_HOST,
     dialect: "postgres",
@@ -23,6 +22,6 @@ if (DB_NAME && DB_USER && DB_PASSWORD && DB_HOST) {
 
   module.exports = { Note, sequelize };
 } else {
-  console.error("local variables are not defined");
+  console.error("Local variables are not defined");
   process.exit(1);
 }
